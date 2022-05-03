a3df0fa2e919502c04aeec170f6cea3ba1d2a11d
SBOXM <53457442+cnhkbbs@users.noreply.github.com>
2022-05-03 13:51:20 +0800

    Add files via upload

---
+++ demos/beijieyeyu/js/main.js
@@ -0,0 +1,23 @@
+// 一言
+fetch('https://v1.hitokoto.cn')
+	.then(function(res) {
+		return res.json();
+	})
+	.then(function(data) {
+		var hitokoto = document.getElementById('hitokoto');
+		hitokoto.innerText = data.hitokoto;
+	})
+	.catch(function(err) {
+		console.error(err);
+	})
+//解决Audio摸鱼问题
+function toggleSound() {
+	var music = document.getElementById("player"); //获取ID  
+	console.log(music);
+	console.log(music.paused);
+	if (music.paused) { //判读是否播放  
+		music.paused = false;
+		music.play(); //没有就播放 
+	}
+}
+setInterval("toggleSound()", 1);
